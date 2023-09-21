b173b1f9042a57a0a52ac650a6ee7e93d725859f
Albert Yang <albertesyang@gmail.com>
2023-09-20 20:39:48 -0400

    add scroll timeout

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -69,12 +69,12 @@ export const Navbar = () => {
     if (location.hash) {
       // TODO: Fix scroll behaviour, scroll only when loaded
       const id = location.hash.substring(1);
-      // setTimeout(() => {
-      //   const el = document.getElementById(id);
-      //   if (el) {
-      //     el.scrollIntoView();
-      //   }
-      // }, 2500);
+      setTimeout(() => {
+        const el = document.getElementById(id);
+        if (el) {
+          el.scrollIntoView();
+        }
+      }, 3000);
     } else {
       window.scrollTo(0, 0);
     }
