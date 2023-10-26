3d03775181ff6170117c303db5dc9d4ab2bcd8a4
Albert Yang <albertesyang@gmail.com>
2023-10-26 00:20:15 -0400

    add compress and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  server: { port: 3000, host: true },
+  site: 'https://www.albertyang.io',
+  integrations: [react(), tailwind(), robotsTxt(), compress()],
+  server: {
+    port: 3000,
+    host: true
+  }
 });
